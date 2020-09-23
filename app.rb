be425648a3b2a214e6819f0d54d19e8b989fb626
kkirby16 <kkirby16@my.whitworth.edu>
2020-09-22 21:27:53 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,22 +1,23 @@
 require_relative "config/environment"
+require "pry"
 
 class App < Sinatra::Base
   get "/reversename/:name" do
-    @user_name = params[:name]
-    @user_name.reverse
+    @name = params[:name]
+    "#{@name.reverse}"
   end
 
   get "/square/:number" do
-    @squared_number = params[:number].to_i * params[:number].to_i
-    "#{@squared_number}"
+    @number = params[:number]
+    "#{@number.to_i * @number.to_i}"
   end
 
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    @string = ""
-    @number.times do @string = @string + @phrase end
-    @string
+    @array = []
+    @number.times do @array << @phrase end
+    @array.join
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
@@ -25,23 +26,24 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    "#{@word1} " + "#{@word2} " + "#{@word3} " + "#{@word4} " + "#{@word5}."
+    @array = []
+    @array << @word1 << @word2 << @word3 << @word4 << @word5
+    @array.join(" ") + "."
   end
 
   get "/:operation/:number1/:number2" do
+    @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    @operation = params[:operation]
-    if @operation == "subtract"
-      @number3 = @number1 - @number2
-    elsif @operation == "add"
-      @number3 = @number1 + @number2
+    if @operation == "add"
+      "#{@number1 + @number2}"
+    elsif @operation == "subtract"
+      "#{@number1 - @number2}"
     elsif @operation == "multiply"
-      @number3 = @number1 * @number2
+      "#{@number1 * @number2}"
     elsif @operation == "divide"
-      @number3 = @number1 / @number2
+      "#{@number1 / @number2}"
     end
-    "#{@number3}"
   end
 end
 
